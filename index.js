3cfdabbbe6969e5805756d6cbf5c4914f851c327
Kasey Knudsen <kaseyknudsen@gmail.com>
2022-12-14 14:59:39 -0800

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -1,70 +1,65 @@
-/* this acts as a client for the mongo server. Requires the mongodb node driver, and from the driver,
-the mongoClient object.
- */
-
 const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert").strict;
 const dboper = require("./operations");
 
-//here we are setting up a connection to the mongo db server
 const url = "mongodb://localhost:27017/";
 
-//nucamp is the database we want to connect to
 const dbname = "nucampsite";
 
-//this connects us to the mongo db server
-/*useUnifiedTopology: true enables a major rewrite and update of the topology layer of the mongo db driver. 
-It is recommended to prevent deprecated warnings from showing up.*/
-//1
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  //checking if the first argument strictly equals the 2nd (err === null)
-  //if they don't match, the application will quit. If they do, we continue on
-  assert.strictEqual(err, null);
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected correctly to server");
 
-  console.log("Connected correctly to server");
-  //this connects us to the nucampsite database on the mongo db server
-  const db = client.db(dbname);
-  //2
-  db.dropCollection("campsites", (err, result) => {
-    assert.strictEqual(err, null);
-    console.log("Dropped Collection:", result);
-    //3
-    dboper.insertDocument(
-      db,
-      { name: "Breadcrumb Trail Campground", description: "Test" },
-      "campsites",
-      (result) => {
+    const db = client.db(dbname);
+
+    db.dropCollection("campsites")
+      .then((result) => {
+        console.log("Dropped Collection:", result);
+      })
+      .catch((err) => console.log("No collection to drop."));
+
+    dboper
+      .insertDocument(
+        db,
+        { name: "Breadcrumb Trail Campground", description: "Test" },
+        "campsites"
+      )
+      .then((result) => {
         console.log("Insert Document:", result.ops);
-        //4
-        dboper.findDocuments(db, "campsites", (docs) => {
-          console.log("Found Documents:", docs);
-          //5
-          dboper.updateDocument(
-            db,
-            { name: "Breadcrumb Trail Campground" },
-            { description: "Updated Test Description" },
-            "campsites",
-            (result) => {
-              console.log("Updated Document Count:", result.result.nModified);
-              //6
-              dboper.findDocuments(db, "campsites", (docs) => {
-                console.log("Found Documents:", docs);
-                //7
-                dboper.removeDocument(
-                  db,
-                  { name: "Breadcrumb Trail Campground" },
-                  "campsites",
-                  (result) => {
-                    console.log("Deleted Document Count:", result.deletedCount);
 
-                    client.close();
-                  }
-                );
-              });
-            }
-          );
-        });
-      }
-    );
-  });
-});
+        return dboper.findDocuments(db, "campsites");
+      })
+      .then((docs) => {
+        console.log("Found Documents:", docs);
+
+        return dboper.updateDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          { description: "Updated Test Description" },
+          "campsites"
+        );
+      })
+      .then((result) => {
+        console.log("Updated Document Count:", result.result.nModified);
+
+        return dboper.findDocuments(db, "campsites");
+      })
+      .then((docs) => {
+        console.log("Found Documents:", docs);
+
+        return dboper.removeDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          "campsites"
+        );
+      })
+      .then((result) => {
+        console.log("Deleted Document Count:", result.deletedCount);
+
+        return client.close();
+      })
+      .catch((err) => {
+        console.log(err);
+        client.close();
+      });
+  })
+  .catch((err) => console.log(err));
--- operations.js
+++ operations.js
@@ -1,30 +1,22 @@
-const assert = require("assert").strict;
-
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
   //collection argument will be a string with a campsite that's stored in the mongodb server
+  /*  when no callback is given, node js driver automatically returns a promise. To do this,
+  we prepend coll.insertOne with return*/
   const coll = db.collection(collection);
-  coll.insertOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.insertOne(document);
 };
-exports.findDocuments = (db, collection, callback) => {
+
+exports.findDocuments = (db, collection) => {
   const coll = db.collection(collection);
-  coll.find().toArray((err, docs) => {
-    assert.strictEqual(err, null), callback(docs);
-  });
+  return coll.find().toArray();
 };
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
   const coll = db.collection(collection);
-  coll.deleteOne(document, (err, result) => {
-    assert.strictEqual(err, null), callback(result);
-  });
+  return coll.deleteOne(document);
 };
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
   const coll = db.collection(collection);
   /* $set is an update operator. the update object will write over existing information.
   The third parameter is for optional configurations, which we don't need so we pass null */
-  coll.updateOne(document, { $set: update }, null, (err, result) => {
-    assert.strictEqual(err, null), callback(result);
-  });
+  return coll.updateOne(document, { $set: update }, null);
 };
